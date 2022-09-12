e02759c9ce7a4812b602e569d46490a769445461
guidomodarelli25 <guidomodarelli25@gmail.com>
2022-09-12 16:27:28 -0300

    format

--- git-update.sh
+++ git-update.sh
@@ -2,44 +2,44 @@
 
 paths="$@"
 
-git_current_branch () {
-  local ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
-  local ret=$?
-  if [[ $ret != 0 ]]; then
-    [[ $ret == 128 ]] && return
-    ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-  fi
-  echo ${ref#refs/heads/}
+git_current_branch() {
+	local ref=$(git symbolic-ref --quiet HEAD 2>/dev/null)
+	local ret=$?
+	if [[ $ret != 0 ]]; then
+		[[ $ret == 128 ]] && return
+		ref=$(git rev-parse --short HEAD 2>/dev/null) || return
+	fi
+	echo ${ref#refs/heads/}
 }
 
-update () {
-  cd "$1"
+update() {
+	cd "$1"
 
-  local diffs=$(git diff --shortstat)
+	local diffs=$(git diff --shortstat)
 
-  printf "\033[94;1m%s\033[0m " "$1"
+	printf "\033[94;1m%s\033[0m " "$1"
 
-  if [ -z "$diffs" ]; then
+	if [ -z "$diffs" ]; then
 
-    git fetch --all --prune --jobs=10 > /dev/null
+		git fetch --all --prune --jobs=10 >/dev/null
 
-    diffs=$(git diff --shortstat origin/"$(git_current_branch)")
+		diffs=$(git diff --shortstat origin/"$(git_current_branch)")
 
-    if [ -z "$diffs" ]; then
-      printf "%s\n" "up-to-date"
-    else
-      printf "\033[93m%s\033[0m\n" "to-update (remote)"
-    fi
+		if [ -z "$diffs" ]; then
+			printf "%s\n" "up-to-date"
+		else
+			printf "\033[93m%s\033[0m\n" "to-update (remote)"
+		fi
 
-  else
-    printf "\033[91m%s\033[0m\n" "has-local-diffs"
-  fi
+	else
+		printf "\033[91m%s\033[0m\n" "has-local-diffs"
+	fi
 }
 
 if [ -z "$paths" ]; then
-  paths=$(ghq list -p)
+	paths=$(ghq list -p)
 fi
 
 for path in ${paths[@]}; do
-  update "$path"
+	update "$path"
 done
